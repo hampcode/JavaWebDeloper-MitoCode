d17ba924bb20515d8976c4f6356d1867564f556c
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-22 08:29:16 -0500

    Inline lambda expression

--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda4/app/Program.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda4/app/Program.java
@@ -2,7 +2,6 @@ package com.mitocode.lambda4.app;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.mitocode.lambda4.entities.Product;
@@ -19,9 +18,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 
 		names.forEach(System.out::println);
 	}
