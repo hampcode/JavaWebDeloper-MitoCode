ca7edb0f14917394f6973b7b9de7363899474595
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-22 08:22:45 -0500

    Inline lambda expression

--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda3/app/Program.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda3/app/Program.java
@@ -19,11 +19,7 @@ public class Program {
 
 		double factor = 1.1;
 
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * factor);
-		};
-
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
 		list.forEach(System.out::println);
 
