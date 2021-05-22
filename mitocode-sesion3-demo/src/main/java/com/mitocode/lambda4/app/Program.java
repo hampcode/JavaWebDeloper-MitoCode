3ef4a8a1ab0b62cba3b9b0e4197698db8eea4e08
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-22 08:28:40 -0500

    Declared lambda expression

--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda4/app/Program.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda4/app/Program.java
@@ -2,6 +2,7 @@ package com.mitocode.lambda4.app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.mitocode.lambda4.entities.Product;
@@ -18,7 +19,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 
 		names.forEach(System.out::println);
 	}
