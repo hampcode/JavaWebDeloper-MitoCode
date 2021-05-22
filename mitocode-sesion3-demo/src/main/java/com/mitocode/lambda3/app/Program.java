db1f1cba1789f6a857192b97acc83362f9112a2c
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-22 08:21:58 -0500

    Declared lambda expression

--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda3/app/Program.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda3/app/Program.java
@@ -2,6 +2,7 @@ package com.mitocode.lambda3.app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import com.mitocode.lambda3.entities.Product;
 
@@ -16,7 +17,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+
+		list.forEach(cons);
 
 		list.forEach(System.out::println);
 
