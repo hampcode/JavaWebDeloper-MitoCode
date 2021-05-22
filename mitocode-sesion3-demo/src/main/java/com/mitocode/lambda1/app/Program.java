3e696b345fc338689927a5936fa93f038a7136f2
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-22 07:40:31 -0500

    Lambda 1

--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda1/app/Program.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda1/app/Program.java
@@ -16,11 +16,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {
+		Comparator<Product> comp = (p1, p2) ->{
 				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
 		};
 
 		list.sort(comp);
