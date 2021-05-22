c749ff37be4f8bc225eaf4d88cfe1c70a17ac31c
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-22 08:10:14 -0500

    Method reference non static

--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda2/app/Program.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda2/app/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		list.removeIf(Product::staticProductPredicate);
+		list.removeIf(Product::nonStaticProductPredicate);
 
 		for (Product p : list) {
 			System.out.println(p);
--- mitocode-sesion3-demo/src/main/java/com/mitocode/lambda2/entities/Product.java
+++ mitocode-sesion3-demo/src/main/java/com/mitocode/lambda2/entities/Product.java
@@ -29,6 +29,10 @@ public class Product {
 	public static boolean staticProductPredicate(Product p) {
 		return p.getPrice() >= 100.0;
 	}
+	
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.0;
+	}
 
 	@Override
 	public String toString() {
