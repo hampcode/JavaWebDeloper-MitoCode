0e2674c710466c2013faaeff52b664232faf671f
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-05-27 15:56:02 -0500

    Ejemplo 3: Encuentra la 5 primeras tareas de lectura ordenadas por fecha de creación

--- mitocode-sesion4-demo/mitocode-sesion4-demo/src/main/java/com/mitocode/programstream/Examples.java
+++ mitocode-sesion4-demo/mitocode-sesion4-demo/src/main/java/com/mitocode/programstream/Examples.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
 import com.mitocode.entities.Task;
 import com.mitocode.entities.TaskType;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 public class Examples {
 
 	public static void main(String[] args) {
@@ -18,20 +21,33 @@ public class Examples {
 		
 		List<Task> distinctTasks = allDistinctTasks(tasks);
 		distinctTasks.forEach(System.out::println);
+		
+		List<String> topNTasks=topN(tasks,3);
+		topNTasks.forEach(System.out::println);
 	}
 
 	// TODO Ejemplo 1: Encuentra todas los títulos de tareas de lectura ordenadas por la fecha de creación
 	private static List<String> allReadingTasks(List<Task> tasks) {
 		 return tasks.stream().
 		            filter(task -> task.getType() == TaskType.READING).
-		            sorted(Comparator.comparing(Task::getCreatedOn).reversed()).
+		            sorted(comparing(Task::getCreatedOn).reversed()).
 		            map(Task::getTitle).
 		            collect(Collectors.toList());
 	}
 	
 	//TODO Ejemplo 2: Encuentra tareas distintas
 	public static List<Task> allDistinctTasks(List<Task> tasks) {
-	    return tasks.stream().distinct().collect(Collectors.toList());
+	    return tasks.stream().distinct().collect(toList());
+	}
+	
+	//TODO Ejemplo 3: Encuentra la 5 primeras tareas de lectura ordenadas por fecha de creación
+	public static List<String> topN(List<Task> tasks, int n){
+	    return tasks.stream().
+	            filter(task -> task.getType() == TaskType.READING).
+	            sorted(comparing(Task::getCreatedOn)).
+	            map(Task::getTitle).
+	            limit(n).
+	            collect(toList());
 	}
 
 }
